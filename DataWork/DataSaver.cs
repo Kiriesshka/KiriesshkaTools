d3162e354ae867f4f7e32769e9dc4ab93d3dd09e
Kiriesshka <kirieshkaabuser@gmail.com>
2024-08-12 15:41:18 +0300

    Фикс непонятного бага

--- DataWork/DataSaver.cs
+++ DataWork/DataSaver.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using System;
-
 namespace KiriesshkaData
 {
     public class ApplicationInfo
@@ -19,7 +18,7 @@ namespace KiriesshkaData
     }
     public class DataSaver
     {
-        public string fileName;
+        public string fileName = "savedData";
         public string fileExtension = ".json";
         public bool prettyPrint;
         private string dataToSave;
@@ -45,7 +44,7 @@ namespace KiriesshkaData
         }
         public void Add(string name, string stringToSave)
         {
-            dataToSave += name + " S:S "+stringToSave + " S:S _STRING_ END\n";
+            dataToSave += name + " S:S "+stringToSave + " S:S  END\n";
         }
         public void Add(string name, int intToSave)
         {
@@ -59,7 +58,7 @@ namespace KiriesshkaData
         {
             foreach (string s in loadedData)
             {
-                if (s.Contains("_STRING_"))
+                if (s.Contains(" S:S "))
                 {
                     List<string> parts = new List<string>(s.Split(" S:S "));
                     if (parts[0] == name)
@@ -74,7 +73,7 @@ namespace KiriesshkaData
         {
             foreach (string s in loadedData)
             {
-                if (s.Contains("_FLOAT_"))
+                if (s.Contains(" F:F "))
                 {
                     List<string> parts = new List<string>(s.Split(" F:F "));
                     if (parts[0] == name)
@@ -89,12 +88,12 @@ namespace KiriesshkaData
         {
             foreach (string s in loadedData)
             {
-                if (s.Contains("_INT_"))
+                if (s.Contains(" I:I "))
                 {
                     List<string> parts = new List<string>(s.Split(" I:I "));
                     if (parts[0] == name)
                     {
-                        return int.Parse(parts[1].Replace(" ", ""));
+                        return int.Parse(parts[1]);
                     }
                 }
             }
@@ -104,7 +103,7 @@ namespace KiriesshkaData
         {
             foreach (string s in loadedData)
             {
-                if (s.Contains("_BOOL_"))
+                if (s.Contains(" B:B "))
                 {
                     List<string> parts = new List<string>(s.Split(" B:B "));
                     if (parts[0] == name)
@@ -120,7 +119,7 @@ namespace KiriesshkaData
         {
             foreach(string s in loadedData)
             {
-                if (s.Contains("_CLASS_"))
+                if (s.Contains(" C:C "))
                 {
                     List<string> parts = new List<string>(s.Split(" C:C "));
                     if(parts[0] == name)
@@ -133,13 +132,16 @@ namespace KiriesshkaData
         }
         public void Save()
         {
+            
             File.WriteAllText(SavePath(), dataToSave);
+
         }
         public void Load()
         {
             if(File.Exists(SavePath()))
             {
                 string data = File.ReadAllText(SavePath());
+                
                 loadedData = new List<string>(data.Split(" END\n"));
             }
             else
@@ -149,19 +151,19 @@ namespace KiriesshkaData
         }
         public string StringifyClass<T>(string name, T obj)
         {
-            return name + " C:C " + JsonUtility.ToJson(obj, prettyPrint) + " C:C _CLASS_ END\n";
+            return name + " C:C " + JsonUtility.ToJson(obj, prettyPrint) + " C:C  END\n";
         }
         public string StringifyBool(string name, bool b)
         {
-            return name + " B:B " + (b ? "true" : "else") + " B:B _BOOL_ END\n";
+            return name + " B:B " + (b ? "true" : "else") + " B:B  END\n";
         }
         public string StringifyFloat(string name, float f)
         {
-            return name + " F:F " + f.ToString() + " F:F _FLOAT_ END\n";
+            return name + " F:F " + f.ToString() + " F:F  END\n";
         }
         public string StringifyInt(string name, int i)
         {
-            return name + " I:I " + i.ToString() + " I:I _INT_ END\n";
+            return name + " I:I " + i.ToString() + " I:I  END\n";
         }
     }
 }
