2095bbbea3321e3ce2f05498d7d301e87f57e70a
Kiriesshka <kirieshkaabuser@gmail.com>
2024-09-08 14:44:18 +0300

    Added sound control script

---
+++ Audio/GameSound.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSound : MonoBehaviour
+{
+    public string audioFolder;
+    public List<string> soundChannels;
+    public List<float> channelsVolumeSettings;
+    public void MakeSound(string soundName, string soundChannel)
+    {
+        if (!soundChannels.Contains(soundName))
+        {
+            Debug.LogError("No sound channel with name: [" + soundChannel + "]");
+            return;
+        }
+        if(channelsVolumeSettings.Count < soundChannels.Count)
+        {
+            Debug.Log("No volume settings for all of sound channels");
+            return;
+        }
+        GameObject soundObj = new GameObject();
+        soundObj.name = soundChannel + "->" + soundName;
+        soundObj.AddComponent<AudioSource>();
+        soundObj.GetComponent<AudioSource>().clip = Resources.Load<AudioClip>(audioFolder + "/"+soundName);
+        soundObj.GetComponent<AudioSource>().volume = channelsVolumeSettings[soundChannels.IndexOf(soundChannel)];
+        soundObj.GetComponent<AudioSource>().Play();
+        soundObj.AddComponent<DestroyAfterAudioStop>();
+    }
+}
+public class DestroyAfterAudioStop : MonoBehaviour
+{
+    private void Update()
+    {
+        if(!GetComponent<AudioSource>().isPlaying) Destroy(gameObject);
+    }
+}
