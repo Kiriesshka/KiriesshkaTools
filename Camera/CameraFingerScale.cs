7811e0cbb7309f2a001966b96812a57fa3de0c50
Kiriesshka <kirieshkaabuser@gmail.com>
2024-09-19 20:09:53 +0300

    Added new camera script

---
+++ Camera/CameraFingerScale.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CameraFingerScale : MonoBehaviour
+{
+    private Camera cam;
+    public float scale;
+    public float minSize;
+    public float maxSize;
+    private bool listenForTouches;
+    private Vector2 firstPoint;
+    private Vector2 secondPoint;
+    private float refernceDistance;
+    private float oldScale;
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+    private void Update()
+    {
+        if(Input.touchCount == 2)
+        {
+            
+                if (listenForTouches)
+                {
+                    firstPoint = Input.GetTouch(0).position;
+                    secondPoint = Input.GetTouch(1).position;
+                    listenForTouches = false;
+                    refernceDistance = Vector2.Distance(firstPoint, secondPoint);        
+                }
+                float dist = Vector2.Distance(Input.GetTouch(0).position, Input.GetTouch(1).position);
+
+                float newSize = Mathf.Lerp(cam.orthographicSize, oldScale + (refernceDistance - dist) * scale, Time.deltaTime * 5);
+
+                if (newSize > maxSize) newSize = maxSize;
+                if (newSize < minSize) newSize = minSize;
+                cam.orthographicSize = newSize;
+
+            
+
+
+        }
+        else
+        {
+            listenForTouches = true;
+            oldScale = cam.orthographicSize;
+        }
+    }
+}
